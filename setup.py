63dbd9c32f6c4cdf2b2897f5e0977e82021ea46b
Tino Pietrassyk <pietrassyk@googlemail.com>
2019-05-07 12:18:48 -0400

    Initial Commit Test in tpcelanscript

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='tpclean',  
+     version='0.1',
+     scripts=['tpclean'] ,
+     author="Tino Pietrassyk",
+     author_email="pietrassyk@gmail.com",
+     description="Custom library of functions for Data Science workflow automation",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Pietrassyk/tpclean",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
